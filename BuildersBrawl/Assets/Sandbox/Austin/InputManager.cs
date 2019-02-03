164635051f53e3c23e6fa44b58a789a40046788d
aschmidt25 <46943909+aschmidt25@users.noreply.github.com>
2019-02-02 16:56:20 -0800

    V1 of the InputManager

---
+++ BuildersBrawl/Assets/Sandbox/Austin/InputManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class InputManager : MonoBehaviour
+{
+    public int playerID;
+    private Player player;
+
+    private Controller activeController;
+
+    //direction player is moving
+    public Vector3 moveVector;
+
+    public bool isMoving = false;
+    public bool isJumping = false;
+    public bool isCharging = false;
+    public bool isPushing = false;
+    public bool isUsingBoard = false;
+    public bool isSlamming = false;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+        
+
+    }
+
+    void Update()
+    {
+        GetInput();
+        ProcessInput();
+    }
+
+    private void GetInput()
+    {
+        
+        //input for left stick
+        moveVector.x = player.GetAxis("Move Horizontal");
+        moveVector.z = player.GetAxis("Move Vertical");
+        moveVector.y = 0f;
+        if (moveVector != Vector3.zero)
+            isMoving = true;
+
+        //input for A, B, X, Y
+        isJumping = player.GetButtonDown("Jump");
+        isCharging = player.GetButtonDown("Charge");
+        isPushing = player.GetButtonDown("Push");
+        isUsingBoard = player.GetButtonDown("Use Board");
+
+        //input for bumpbers and triggers
+        isSlamming = player.GetButtonDown("Board Slam");
+        if (player.GetAxis("Board Slam") != 0)
+            isSlamming = true;
+
+    }
+
+    private void ProcessInput()
+    {
+        
+        if (isMoving)
+            Debug.Log("Moving " + moveVector.x + " in the X direction and " + moveVector.z + " in the Z direction");
+
+        if (isJumping)
+            Debug.Log("Jump");
+
+        if (isCharging)
+            Debug.Log("Charge");
+
+        if (isPushing)
+            Debug.Log("Push");
+
+        if (isUsingBoard)
+            Debug.Log("Using Board");
+
+        if (isSlamming)
+            Debug.Log("Slam");
+    }
+}
