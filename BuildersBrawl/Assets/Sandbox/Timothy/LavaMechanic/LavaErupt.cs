967270b18b395186f650756f11f15161bc96fcea
Timothy Robinson <timothyr218@gmail.com>
2019-02-10 20:36:28 -0800

    Started Lava Mechanic Testing
    - Created Lava Mechanic Testing Scene
    - Created LavaErupt Script to simulate the most basic lava eruption and dissipation

---
+++ BuildersBrawl/Assets/Sandbox/Timothy/LavaMechanic/LavaErupt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaErupt : MonoBehaviour
+{
+    public Vector3 targetPos;
+    public float movePerFrame;
+    public float dissolveTime;
+    public float cooldownTime;
+
+    private Vector3 startingPos;
+
+    private void Start()
+    {
+        startingPos = transform.position;
+        StartCoroutine("LavaBoil");
+    }
+
+    private IEnumerator LavaBoil()
+    {
+        bool dissolveLava = false;
+
+        while (true)
+        {
+            if (!dissolveLava)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, movePerFrame * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, targetPos) <= .2f)
+                {
+                    dissolveLava = true;
+                    yield return new WaitForSeconds(dissolveTime);
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startingPos, movePerFrame * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, startingPos) <= .2f)
+                {
+                    dissolveLava = false;
+                    yield return new WaitForSeconds(cooldownTime);
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
