6ad46e3e5d727fc5478479decf0b0a11100fd35a
CAGD-user <CAGD-user@csuchico.edu>
2019-02-19 16:47:29 -0800

    Created Wind Mechanic
    - Created WindMechanic Test Scene
    - Created WindMechanic Script

---
+++ BuildersBrawl/Assets/Sandbox/Timothy/WindMechanic/WindMechanic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMechanic : MonoBehaviour
+{
+    public enum WindDirection
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public WindDirection windDirection;
+    public bool windInProgress = false;
+    public float windSpeed;
+    public GameObject player;
+
+    private void Update()
+    {
+        if (windInProgress)
+        {
+            switch (windDirection)
+            {
+                case WindDirection.North:
+                    player.transform.position += Vector3.forward * windSpeed * Time.deltaTime;
+                    break;
+                case WindDirection.East:
+                    player.transform.position += Vector3.right * windSpeed * Time.deltaTime;
+                    break;
+                case WindDirection.South:
+                    player.transform.position += Vector3.back * windSpeed * Time.deltaTime;
+                    break;
+                case WindDirection.West:
+                    player.transform.position += Vector3.left * windSpeed * Time.deltaTime;
+                    break;
+                default:
+                    Debug.Log("This direction does not exist!");
+                    break;
+            }
+        }
+    }
+
+}
