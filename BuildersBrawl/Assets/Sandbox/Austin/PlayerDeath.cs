25522c5e77a9cbfbbeced6461f1e348f168ae062
CAGD-user <CAGD-user@csuchico.edu>
2019-02-28 16:03:04 -0800

    Started Work on FlashyPoints

--- BuildersBrawl/Assets/Sandbox/Austin/PlayerDeath.cs
+++ BuildersBrawl/Assets/Sandbox/Austin/PlayerDeath.cs
@@ -29,6 +29,7 @@ public class PlayerDeath : MonoBehaviour
     public float respawnTime = 5f;
     public float timeToWaitAfterPushed = 0.5f;
     private GameObject otherPlayer;
+    private Vector3 playerdeathPos;
 
     [HideInInspector]
     public bool playerDead;
@@ -74,7 +75,7 @@ public class PlayerDeath : MonoBehaviour
 
         //set start avgposition/distance (before moved)
         cc.GetComponent<CameraController>().SetDeathStartValues();
-
+        playerdeathPos = transform.position;
         //move player
         this.gameObject.transform.position = spawnPoint.transform.position;
 
