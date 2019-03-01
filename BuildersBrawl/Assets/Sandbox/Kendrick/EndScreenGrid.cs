ab9d642c8f72cb1ce70e718a4e211206db3e06b0
Kendrick Short <kshort13@mail.csuchico.edu>
2019-02-28 16:05:40 -0800

    End_Screen
    -Created End Screen & related scripts in my Sandbox

---
+++ BuildersBrawl/Assets/Sandbox/Kendrick/EndScreenGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour
+{
+    public int rows = 5;
+    public int columns = 3;
+    public GameObject inputFieldPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*RectTransform parentRect = gameObject.GetComponent<RectTransform>();
+        GridLayoutGroup gridLayout = gameObject.GetComponent<GridLayoutGroup>();
+        gridLayout.cellSize = new Vector2(parentRect.rect.width / columns, parentRect.rect.height / rows);
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                GameObject inputField = Instantiate(inputFieldPrefab);
+                inputField.transform.SetParent(gameObject.transform, false);
+            }
+        }*/
+        Populate();
+    }
+
+    void Populate()
+    {
+        GameObject newObj;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                newObj = (GameObject)Instantiate(inputFieldPrefab, transform);
+                newObj.GetComponent<Image>().color = Random.ColorHSV();
+            }
+        }
+    }
+}
