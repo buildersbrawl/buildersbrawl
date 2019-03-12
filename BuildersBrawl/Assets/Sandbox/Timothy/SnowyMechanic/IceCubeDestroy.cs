7b476fe3d8688dd0dae34175a217ebd39f35955b
CAGD-user <CAGD-user@csuchico.edu>
2019-03-12 16:59:52 -0700

    Started Working on Icy Mechanic
    - Started Icy Mechanic script for Snow Level
    - Added Taylor's Level Design Assests for testing

---
+++ BuildersBrawl/Assets/Sandbox/Timothy/SnowyMechanic/IceCubeDestroy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCubeDestroy : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float moveAmount = 0.2f;
+    private float startTimer;
+    private float timeTilDestruction = 5;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        startTimer = Time.time;
+    }
+    private void Update()
+    {
+        float timePassed = Time.time - startTimer;
+        float movementPassed = 0;
+        if (timePassed >= timeTilDestruction)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            if (movementPassed <= moveAmount)
+            {
+                transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+                movementPassed = transform.position.y - startPos.y;
+            }
+        }
+    }
+}
---
+++ BuildersBrawl/Assets/Sandbox/Timothy/SnowyMechanic/IcyMechanic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcyMechanic : MonoBehaviour
+{
+    public GameObject iceCubePrefab;
+    private Vector3 deathPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            deathPos = other.gameObject.transform.position + new Vector3(0, -1.1f);
+            //Debug.Log("DeathPos: " + deathPos); //12.3,5.3,90.5
+            other.gameObject.GetComponent<PlayerDeath>().KillMe();
+            Instantiate(iceCubePrefab, deathPos, Quaternion.identity);
+        }
+    }
+}
