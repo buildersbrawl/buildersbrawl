3ce26928d40db34629cace175b459d92f82f6e6f
CAGD-user <CAGD-user@csuchico.edu>
2019-03-14 15:57:20 -0700

    Continued Work on IcyMechanic
    - Created 4 Prefabs for Testing
    - Edited Code to Allow for More Prefabs

--- BuildersBrawl/Assets/Sandbox/Timothy/SnowyMechanic/IcyMechanic.cs
+++ BuildersBrawl/Assets/Sandbox/Timothy/SnowyMechanic/IcyMechanic.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IcyMechanic : MonoBehaviour
 {
-    public GameObject iceCubePrefab;
+    public GameObject[] iceCubePrefab;
     private Vector3 deathPos;
 
     private void OnTriggerEnter(Collider other)
@@ -14,7 +15,20 @@ public class IcyMechanic : MonoBehaviour
             deathPos = other.gameObject.transform.position + new Vector3(0, -1.1f);
             //Debug.Log("DeathPos: " + deathPos); //12.3,5.3,90.5
             other.gameObject.GetComponent<PlayerDeath>().KillMe();
-            Instantiate(iceCubePrefab, deathPos, Quaternion.identity);
+            //Instantiate(iceCubePrefab, deathPos, Quaternion.identity);
+            InstantiateIceCube(other.gameObject, deathPos);
         }
+    }
+
+    private void InstantiateIceCube(GameObject player, Vector3 deathPos)
+    {
+        if (player == GameManager.S.player1)//Blue
+            Instantiate(iceCubePrefab[0], deathPos, Quaternion.identity);
+        else if (player == GameManager.S.player2)//Red
+            Instantiate(iceCubePrefab[1], deathPos, Quaternion.identity);
+        else if (player == GameManager.S.player3)//Yellow
+            Instantiate(iceCubePrefab[2], deathPos, Quaternion.identity);
+        else if (player == GameManager.S.player4)//Purple
+            Instantiate(iceCubePrefab[3], deathPos, Quaternion.identity);
     }
 }
