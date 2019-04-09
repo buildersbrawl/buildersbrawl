5f2216a862ce1cd71b111ef666cde75cb487e800
CAGD-user <CAGD-user@csuchico.edu>
2019-04-09 16:51:13 -0700

    Scripted RespawnArrows
    - Respawn Arrows turn on and off by script
    - Fixed Respawn Arrow Prefab

---
+++ BuildersBrawl/Assets/Sandbox/Timothy/VolcanoTesting/RespawnArrows.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnArrows : MonoBehaviour
+{
+    public GameObject respawnArrow;
+    private void Update()
+    {
+        if (!(this.GetComponent<PlayerDeath>().playerDead))
+        {
+            respawnArrow.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            respawnArrow.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
