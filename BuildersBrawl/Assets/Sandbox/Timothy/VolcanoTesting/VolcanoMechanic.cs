c3f508a7231993b5ab5c969e2439859ffc76e244
CAGD-user <CAGD-user@csuchico.edu>
2019-04-11 15:50:25 -0700

    Started Refining LavaMechanic
    - Made new Volcano Mechanic Script

---
+++ BuildersBrawl/Assets/Sandbox/Timothy/VolcanoTesting/VolcanoMechanic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoMechanic : MonoBehaviour
+{
+    public float timeUntilErupt;
+    public float movePerFrame; //Lava Movement Per Frame
+
+    private Vector3 lavaStartingPos;
+
+    private void Start()
+    {
+        lavaStartingPos = transform.position;
+    }
+}
