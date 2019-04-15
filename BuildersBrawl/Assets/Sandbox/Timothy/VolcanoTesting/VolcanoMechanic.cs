a2c006c2dc87f0345195647d07a17336d485a394
CAGD-user <CAGD-user@csuchico.edu>
2019-04-15 15:22:54 -0700

    Continued Work on Lava Mechanic
    - Continued work on refining Lava Mechanic

--- BuildersBrawl/Assets/Sandbox/Timothy/VolcanoTesting/VolcanoMechanic.cs
+++ BuildersBrawl/Assets/Sandbox/Timothy/VolcanoTesting/VolcanoMechanic.cs
@@ -6,11 +6,34 @@ public class VolcanoMechanic : MonoBehaviour
 {
     public float timeUntilErupt;
     public float movePerFrame; //Lava Movement Per Frame
+    public bool eruptCooldown = true;
 
     private Vector3 lavaStartingPos;
+    private float startTimer;
+
 
     private void Start()
     {
         lavaStartingPos = transform.position;
+        startTimer = Time.time;
+    }
+
+    private void Update()
+    {
+        
+        if (!eruptCooldown)
+        {
+            Debug.Log("Eruption In Progress!");
+        }
+        else
+        {
+            Debug.Log("The Volcano Lies Dormant.");
+            float timePassed = Time.time - startTimer;
+
+            if (timePassed >= timeUntilErupt)
+            {
+                eruptCooldown = false; //End the Cooldown (Volcano is now active!)
+            }
+        }
     }
 }
