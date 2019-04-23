0f75cfeca4e0c05bd818ae4c39426963040fc045
TaccaT17 <tom@crammond.name>
2019-04-23 11:44:45 -0700

    BBBuild_5_Pt2

---
+++ BuildersBrawl/Assets/Materials/Volcano/ScrollTex.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTex : MonoBehaviour
+{
+
+    public float ScrollX = 0.5f;
+    public float ScrollY = 0.5f;
+    private float OffsetX;
+    private float OffsetY;
+
+    void Update()
+    {
+        OffsetX = Time.time * ScrollX * Time.deltaTime;
+        OffsetY = Time.time * ScrollY * Time.deltaTime;
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+    }
+}
